2cd5fb021b31f5d0addfa41b82c6d1ab9f6b1790
prajval kanth <31241057+prajvalkanth@users.noreply.github.com>
2018-03-22 20:43:10 +0530

    Add files via upload

---
+++ max6675.h
@@ -0,0 +1,18 @@
+#if ARDUINO >= 100
+ #include "Arduino.h"
+#else
+ #include "WProgram.h"
+#endif
+
+class MAX6675 {
+ public:
+  MAX6675(int8_t SCLK, int8_t CS, int8_t MISO);
+
+  double readCelsius(void);
+  double readFahrenheit(void);
+  // For compatibility with older versions:
+  double farenheit(void) { return readFahrenheit(); }
+ private:
+  int8_t sclk, miso, cs;
+  uint8_t spiread(void);
+};
